50d99ad4cf85c6ad2f196b6b628099571753bcee
Francisco Vasquez <fjvasquez8@misena.edu.co>
2023-06-29 13:57:31 -0500

    material
    material

--- src/config/global.js
+++ src/config/global.js
@@ -80,6 +80,11 @@ export default {
       },
       {
         icono: 'fas fa-download',
+        titulo: 'Descargar PDF',
+        download: 'downloads/CFA_11_224204.pdf',
+      },
+      {
+        icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
       },
